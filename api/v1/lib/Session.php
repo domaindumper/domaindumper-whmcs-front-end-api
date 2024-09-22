c579acb7103929006db9674b8e1f96c855f230bf
Shiv Singh <shiv@srapsware.com>
2024-09-22 15:16:49 +0530

    Update Session.php to use tblusers table instead of auth_tokens table

--- api/v1/lib/Session.php
+++ api/v1/lib/Session.php
@@ -2,32 +2,32 @@
 
 function CreateSession($authToken, $client_id)
 {
-    Illuminate\Database\Capsule\Manager::table('auth_tokens')
+    Illuminate\Database\Capsule\Manager::table('tblusers')
         ->updateOrInsert(
-            ['client_id' => $client_id],
+            ['id' => $client_id],
             ['auth_token' => $authToken, 'created_at' => date('Y-m-d H:i:s')]
         );
 }
 
 function DestroySession($authToken)
 {
-    Illuminate\Database\Capsule\Manager::table('auth_tokens')->where('auth_token', $authToken)->delete();
+    Illuminate\Database\Capsule\Manager::table('tblusers')->where('auth_token', $authToken)->delete();
 }
 function UpdateSession($authToken)
 {
-    $user = Illuminate\Database\Capsule\Manager::table('auth_tokens')->where('auth_token', $authToken)->first();
+    $user = Illuminate\Database\Capsule\Manager::table('tblusers')->where('auth_token', $authToken)->first();
 }
 
 function GetSession($authToken)
 {
-    $user = Illuminate\Database\Capsule\Manager::table('auth_tokens')->where('auth_token', $authToken)->first();
+    $user = Illuminate\Database\Capsule\Manager::table('tblusers')->where('auth_token', $authToken)->first();
 
     return $user;
 }
 
 function isSessionActive($authToken)
 {
-    if (Illuminate\Database\Capsule\Manager::table('auth_tokens')->where('auth_token', $authToken)->exists()) {
+    if (Illuminate\Database\Capsule\Manager::table('tblusers')->where('auth_token', $authToken)->exists()) {
         return true;
     }else{
         return false;
