aee37560d9d8a01bdf9a3f6300417fa11f7e13bc
Shiv Singh <shiv@srapsware.com>
2024-10-07 18:02:28 +0530

    Refactor session handling to allow OPTIONS request in login API

--- api/v2/user/update/index.php
+++ api/v2/user/update/index.php
@@ -11,23 +11,55 @@ require $_SERVER['DOCUMENT_ROOT'] . '/api/v2/lib/Session.php';
 
 $ca = new ClientArea();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-    $authToken = !empty($_REQUEST['authToken']) ? $_REQUEST['authToken'] : '';
-    $firstname = !empty($_REQUEST['firstname']) ? $_REQUEST['firstname'] : '';
-    $lastname = !empty($_REQUEST['lastname']) ? $_REQUEST['lastname'] : '';
-    $email = !empty($_REQUEST['email']) ? $_REQUEST['email'] : '';
+    $data = json_decode(file_get_contents('php://input'), associative: true);
+
+    $authToken = !empty($data['authToken']) ? $data['authToken'] : '';
+    $firstname = !empty($data['firstname']) ? $data['firstname'] : '';
+    $lastname = !empty($data['lastname']) ? $data['lastname'] : '';
+    $companyname = !empty($data['companyname']) ? $data['companyname'] : '';
+    $email = !empty($data['email']) ? $data['email'] : '';
+    $address1 = !empty($data['address1']) ? $data['address1'] : '';
+    $address2 = !empty($data['address2']) ? $data['address2'] : '';
+    $city = !empty($data['city']) ? $data['city'] : '';
+    $state = !empty($data['state']) ? $data['state'] : '';
+    $postcode = !empty($data['postcode']) ? $data['postcode'] : '';
+    $country = !empty($data['country']) ? $data['country'] : '';
+    $phonenumber = !empty($data['phonenumber']) ? $data['phonenumber'] : '';
+
+    $email_preferences[general] = !empty($data[email_preferences[general]]) ? $data[email_preferences[general]] : '';
+    $email_preferences[product] = !empty($data[email_preferences[product]]) ? $data[email_preferences[product]] : '';
+    $email_preferences[invoice] = !empty($data[email_preferences[invoice]]) ? $data[email_preferences[invoice]] : '';
+    $email_preferences[support] = !empty($data[email_preferences[support]]) ? $data[email_preferences[support]] : '';
+    $email_preferences[affiliate] = !empty($data[email_preferences[affiliate]]) ? $data[email_preferences[affiliate]] : '';
+
+    $marketingoptin = !empty($data['marketingoptin']) ? $data['marketingoptin'] : '';
 
     if (isActiveSession($authToken)) {
 
         $user_id = GetSession($authToken);
 
-        $command = 'UpdateUser';
+        $command = 'UpdateClient';
         $postData = array(
-            'user_id' => $user_id,
+            'clientid' => $user_id,
             'firstname' => $firstname,
             'lastname' => $lastname,
+            'companyname' => $companyname,
             'email' => $email,
+            'address1' => $address1,
+            'address2' => $address2,
+            'city' => $city,
+            'state' => $state,
+            'postcode' => $postcode,
+            'country' => $country,
+            'phonenumber' => $phonenumber,
+            'email_preferences[general]' => $email_preferences[general],
+            'email_preferences[product]' => $email_preferences[product],
+            'email_preferences[invoice]' => $email_preferences[invoice],
+            'email_preferences[support]' => $email_preferences[support],
+            'email_preferences[affiliate]' => $email_preferences[affiliate],
+            'marketingoptin' => $marketingoptin,
         );
 
         $results = localAPI($command, $postData);
